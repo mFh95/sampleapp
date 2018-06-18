d4b9e1451e8e046a8a164fb6281cca731510f57d
Mustafe Ahmed Farah <s305037@stud.hioa.no>
2018-06-18 03:36:18 +0200

    avaa

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ t = np.arange(0.0, 2.0, 0.01)
 s = 1 + np.sin(2*np.pi*t)
 plt.plot(t, s)
 
+print("frf")
 plt.xlabel('time (s)')
 plt.ylabel('voltage (mV)')
 plt.title('About as simple as it gets, fafa')
