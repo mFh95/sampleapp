ef1d45c758df33b8bda6a5626d5ec0a1900a6275
Mustafe Ahmed Farah <s305037@stud.hioa.no>
2018-06-19 21:23:04 +0200

    fa

--- test.py
+++ test.py
@@ -1,15 +1,48 @@
+"""
+=====================================
+Custom tick formatter for time series
+=====================================
 
-import matplotlib.pyplot as plt
+When plotting time series, e.g., financial time series, one often wants
+to leave out days on which there is no data, i.e. weekends.  The example
+below shows how to use an 'index formatter' to achieve the desired plot
+"""
+from __future__ import print_function
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+import matplotlib.cbook as cbook
+import matplotlib.ticker as ticker
 
-t = np.arange(0.0, 2.0, 0.01)
-s = 1 + np.sin(2*np.pi*t)
-plt.plot(t, s)
+datafile = cbook.get_sample_data('aapl.csv', asfileobj=False)
+print('loading %s' % datafile)
+r = mlab.csv2rec(datafile)
 
-print("frf")
-plt.xlabel('time (s)')
-plt.ylabel('voltage (mV)')
-plt.title('About as simple as it gets, fafa')
-plt.grid(True)
-plt.savefig("test.png")
+r.sort()
+r = r[-30:]  # get the last 30 days
+
+
+# first we'll do it the default way, with gaps on weekends
+fig, axes = plt.subplots(ncols=2, figsize=(8, 4))
+ax = axes[0]
+ax.plot(r.date, r.adj_close, 'o-')
+ax.set_title("Default")
+fig.autofmt_xdate()
+
+# next we'll write a custom formatter
+N = len(r)
+ind = np.arange(N)  # the evenly spaced plot indices
+
+
+def format_date(x, pos=None):
+    thisind = np.clip(int(x + 0.5), 0, N - 1)
+    return r.date[thisind].strftime('%Y-%m-%d')
+
+
+ax = axes[1]
+ax.plot(ind, r.adj_close, 'o-')
+ax.xaxis.set_major_formatter(ticker.FuncFormatter(format_date))
+ax.set_title("Custom tick formatter")
+fig.autofmt_xdate()
+
 plt.show()
